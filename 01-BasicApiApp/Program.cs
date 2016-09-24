41358c997bf124bb4f10fd541ae8332992cf0b51
Wahid Alimi <wahid.alimi@gmail.com>
2016-09-24 11:26:17 -0400

    Added Startup.cs and configured Program.cs to use Startup.cs

---
+++ 01-BasicApiApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
