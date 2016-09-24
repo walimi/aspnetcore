1c89547f1e0c268fe0077107dfe6e9a7d76596b8
Wahid Alimi <wahid.alimi@gmail.com>
2016-09-24 13:22:42 -0400

    Configured logging.

--- 01-BasicApiApp/Startup.cs
+++ 01-BasicApiApp/Startup.cs
@@ -1,11 +1,29 @@
+using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Logging;
 
 namespace ConsoleApplication
 {
     public class Startup
     {
+
+        public IConfigurationRoot Configuration { get; }
+        public string EnvironmentName { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder() // Collection of sources for read/write key/value pairs
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            EnvironmentName = env.EnvironmentName;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -16,9 +34,17 @@ namespace ConsoleApplication
         // This method gets called by the runtime, after ConfigureServices, and is required. Use this method to configure the HTTP request pipeline.
         // IApplicationBuilder is required; provides the mechanisms to configure an application’s request pipeline.
         // Middleware is configured here.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IHostingEnvironment env, IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            var startupLogger = loggerFactory.CreateLogger<Startup>();
+
             app.UseMvc();
+
+            startupLogger.LogDebug("Debug output!");
+            startupLogger.LogInformation("Application startup complete!");
+            startupLogger.LogTrace("Trace output!");
+            startupLogger.LogError("Error output!");
         }
     }
 }
